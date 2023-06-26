ba6cd49315250b0dfd6aa496f8ce74bbc47a1e15
Spencer A Warren <spencer.warren.19@cnu.edu>
2023-06-26 16:42:56 -0400

    Working on Calculator

--- FrontEnd/src/app/calculator/calculator.component.ts
+++ FrontEnd/src/app/calculator/calculator.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { HostListener, Component } from "@angular/core";
 
 @Component({
   selector: 'app-calculator',
@@ -9,12 +9,14 @@ export class CalculatorComponent {
 
   grid: string[][][] = 
   [
-    [['C',   'red' ], ['CE', 'red'], ['%', 'charcoal'], ['/', 'charcoal'] ],
-    [['7',   'blue'], ['8', 'blue'], ['9', 'blue'],     ['*', 'charcoal'] ],
+    [['%', 'charcoal'], ['/', 'charcoal'], ['C',   'red' ], ['CE', 'red']],
+    [['7',   'blue'], ['8', 'blue'], ['9', 'blue'],     [Operator.Multiply, 'charcoal'] ],
     [['4',   'blue'], ['5', 'blue'], ['6', 'blue'],     ['+', 'charcoal'] ],
     [['1',   'blue'], ['2', 'blue'], ['3', 'blue'],     ['-', 'charcoal'] ],
-    [['(-)', 'blue'], ['0', 'blue'], ['.', 'charcoal'], ['=', 'charcoal'] ]
+    [['(-)', 'blue'], ['0', 'blue'], ['.', 'charcoal'], ['=', 'red'] ],
+    [['x\u207f', 'blue'], ['x^2', 'blue'], ['x^3', 'blue'], ['x^4', 'blue']]
   ];
+  
 
   equations: string[] = [
     '1+1',
@@ -29,10 +31,26 @@ export class CalculatorComponent {
 
   constructor() { }
 
+  @HostListener('window:keydown', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    switch (event.key) {
+      case 'Enter':
+        this.onButtonClick(Operator.Equals);
+        return;
+      case 'Backspace':
+        this.C();
+        return;
+    }
+    if (event.key.match(/[0-9]/) || event.key.match(/[\+\-\*\/]/) || event.key === '.') {
+      this.onButtonClick(event.key);
+    }
+  }
+
   onButtonClick(value: any) {
+    console.log(value);
     switch (value) {
       case Operator.Equals:
-        this.result = eval(this.equationString);
+        this.equals();
         return;
       case Operator.CE:
         this.CE();
@@ -44,8 +62,7 @@ export class CalculatorComponent {
         this.handleDecimal();
         return;
     }
-    if (-0
-      )
+   
     this.equationString += value;
   }
 
@@ -72,7 +89,17 @@ export class CalculatorComponent {
   }
 
   eval(equation: string) {
-    return eval(equation);
+    let equationCopy = String(equation);
+    equationCopy = equationCopy.replaceAll(/\%/g,"/100");
+    equationCopy = equationCopy.replaceAll(Operator.Multiply,"*");
+    return eval(equationCopy);
+  }
+
+  equals() {
+    if (this.equationString === '') return;
+    this.result = this.eval(this.equationString);
+    this.equations.push(this.equationString);
+    this.equationString = '';
   }
 
 }
@@ -83,8 +110,9 @@ enum Operator {
   NaN = 'NaN',
   Add = '+',
   Subtract = '-',
-  Multiply = '*',
+  Multiply = '\u00D7',
   Divide = '/',
   Equals = '=',
   Decimal = '.',
+  Exponent = '^',
 }
